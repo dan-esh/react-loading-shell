354874de29b8703446b265c6a4bdf4cd768d5b99
Danesh Abeyratne <td.abeyratne@gmail.com>
2019-11-01 14:14:29 +1300

    storybook v1

---
+++ index.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Ellipsis, Ring, Ripple } from './'
+
+export default { title: 'React-CSS-Spinners' }
+
+export const ellipsis = () => <Ellipsis />
+
+export const ring = () => <Ring />
+
+export const ripple = () => <Ripple />
